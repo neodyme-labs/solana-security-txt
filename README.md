b08a4316cca2f86f977cb0b13fc57bdc93406227
CherryWorm <nico.gruendel@web.de>
2022-04-05 12:34:09 +0200

    only create link section on bpf arch

--- README.md
+++ README.md
@@ -135,9 +135,9 @@ Literally all this crate does is define a single macro:
 #[macro_export]
 macro_rules! security_txt {
     ($($name:ident: $value:expr),*) => {
+        #[cfg_attr(target_arch = "bpf", link_section = ".security.txt")]
         #[allow(dead_code)]
         #[no_mangle]
-        #[link_section = ".security.txt"]
         pub static security_txt: &str = concat! {
             "=======BEGIN SECURITY.TXT V1=======\0",
             $(stringify!($name), "\0", $value, "\0",)*
--- security-txt/src/lib.rs
+++ security-txt/src/lib.rs
@@ -131,9 +131,9 @@
 //! #[macro_export]
 //! macro_rules! security_txt {
 //!     ($($name:ident: $value:expr),*) => {
+//!         #[cfg_attr(target_arch = "bpf", link_section = ".security.txt")]
 //!         #[allow(dead_code)]
 //!         #[no_mangle]
-//!         #[link_section = ".security.txt"]
 //!         pub static security_txt: &str = concat! {
 //!             "=======BEGIN SECURITY.TXT V1=======\0",
 //!             $(stringify!($name), "\0", $value, "\0",)*
@@ -183,9 +183,9 @@ pub const SECURITY_TXT_END: &str = "=======END SECURITY.TXT V1=======\0";
 #[macro_export]
 macro_rules! security_txt {
     ($($name:ident: $value:expr),*) => {
+        #[cfg_attr(target_arch = "bpf", link_section = ".security.txt")]
         #[allow(dead_code)]
         #[no_mangle]
-        #[link_section = ".security.txt"]
         pub static security_txt: &str = concat! {
             "=======BEGIN SECURITY.TXT V1=======\0",
             $(stringify!($name), "\0", $value, "\0",)*
