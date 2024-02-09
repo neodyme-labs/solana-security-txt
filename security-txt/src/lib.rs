6e12e380539de93e1e62249ce93d76490deee433
Johnson1s <155325576+Johnson1s@users.noreply.github.com>
2024-02-09 01:18:57 +0100

    Revert "Adding doc comments to macro lines"

--- security-txt/src/lib.rs
+++ security-txt/src/lib.rs
@@ -5,19 +5,15 @@ mod parser;
 #[cfg(feature = "parser")]
 pub use crate::parser::*;
 
-/// Constant for the beginning of the security.txt file.
 pub const SECURITY_TXT_BEGIN: &str = "=======BEGIN SECURITY.TXT V1=======\0";
-/// Constant for the end of the security.txt file.
 pub const SECURITY_TXT_END: &str = "=======END SECURITY.TXT V1=======\0";
 
 #[macro_export]
-/// Create a static string containing the security.txt file.
 macro_rules! security_txt {
     ($($name:ident: $value:expr),*) => {
         #[cfg_attr(target_arch = "bpf", link_section = ".security.txt")]
         #[allow(dead_code)]
         #[no_mangle]
-        /// Static string containing the security.txt file.
         pub static security_txt: &str = concat! {
             "=======BEGIN SECURITY.TXT V1=======\0",
             $(stringify!($name), "\0", $value, "\0",)*
